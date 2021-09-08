30afced4e1e505e69b7170eca1f0e85dc2ea0dc3
chris moore <christopher.moore@revature.net>
2021-09-08 10:25:09 -0700

    Adds some completed coding challenges

---
+++ Coding_Challenges/6_FlowControl/6_FlowControl/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace _6_FlowControl
+{
+  public class Program
+  {
+
+    public static string username { get; set; }
+    public static string password { get; set; }
+    static void Main(string[] args)
+    {
+
+      int temp = GetValidTemperature();
+      Console.WriteLine(temp);
+      GiveActivityAdvice(temp);
+
+      Register();
+      if (Login())
+      {
+        Console.WriteLine("Correct input");
+      }
+
+      GetTemperatureTernary(temp);
+
+
+    }
+
+    /// <summary>
+    /// This method gets a valid temperature between -40 asnd 135 inclusive 
+    /// and returns the valid int. 
+    /// </summary>
+    /// <returns></returns>
+    public static int GetValidTemperature()
+    {
+      // throw new NotImplementedException($"GetValidTemperature() has not been implemented.");
+      int temp = 136;
+      bool unsuccessful;
+      do
+      {
+        Console.WriteLine("Enter a valid number between -40 and 135: ");
+        string v = Console.ReadLine();
+        unsuccessful = int.TryParse(v, out temp);
+      } while (!unsuccessful || temp < -40 || temp > 135);
+
+      Console.WriteLine("Correct!");
+      return temp;
+    }
+
+    /// <summary>
+    /// This method has one int parameter
+    /// It prints outdoor activity advice and temperature opinion to the console 
+    /// based on 20 degree increments starting at -20 and ending at 135 
+    /// n < -20, Console.Write("hella cold");
+    /// -20 <= n < 0, Console.Write("pretty cold");
+    ///  0 <= n < 20, Console.Write("cold");
+    /// 20 <= n < 40, Console.Write("thawed out");
+    /// 40 <= n < 60, Console.Write("feels like Autumn");
+    /// 60 <= n < 80, Console.Write("perfect outdoor workout temperature");
+    /// 80 <= n < 90, Console.Write("niiice");
+    /// 90 <= n < 100, Console.Write("hella hot");
+    /// 100 <= n < 135, Console.Write("hottest");
+    /// </summary>
+    /// <param name="temp"></param>
+    public static void GiveActivityAdvice(int temp)
+    {
+      //throw new NotImplementedException($"GiveActivityAdvice() has not been implemented.");
+      if (temp < -20)
+      {
+        Console.Write("hella cold\n");
+      }
+      else if (temp >= -20 && temp < 0)
+      {
+        Console.Write("pretty cold\n");
+      }
+      else if (temp >= 0 && temp < 20)
+      {
+        Console.Write("cold\n");
+      }
+      else if (temp >= 20 && temp < 40)
+      {
+        Console.Write("thawed out\n");
+      }
+      else if (temp >= 40 && temp < 60)
+      {
+        Console.Write("feels like Autumn\n");
+      }
+      else if (temp >= 60 && temp < 80)
+      {
+        Console.Write("perfect outdoor workout temperature\n");
+      }
+      else if (temp >= 80 && temp < 90)
+      {
+        Console.Write("niiice\n");
+      }
+      else if (temp >= 90 && temp < 100)
+      {
+        Console.Write("hella hot\n");
+      }
+      else if (temp >= 100 && temp < 135)
+      {
+        Console.Write("hottest\n");
+      }
+    }
+
+    /// <summary>
+    /// This method gets a username and password from the user
+    /// and stores that data in the global variables of the 
+    /// names in the method.
+    /// </summary>
+    public static void Register()
+    {
+      // throw new NotImplementedException($"Register() has not been implemented.");
+      Console.WriteLine("Enter you username: ");
+      username = Console.ReadLine();
+      Console.WriteLine("Enter your password: ");
+      password = Console.ReadLine();
+
+
+    }
+
+    /// <summary>
+    /// This method gets username and password from the user and
+    /// compares them with the username and password names provided in Register().
+    /// If the password and username match, the method returns true. 
+    /// If they do not match, the user is reprompted for the username and password
+    /// until the exact matches are inputted.
+    /// </summary>
+    /// <returns></returns>
+    public static bool Login()
+    {
+      // throw new NotImplementedException($"Login() has not been implemented.");
+      string inputUsername;
+      string InputPassword;
+
+      Console.WriteLine("Please enter the username and password that you created: ");
+      do
+      {
+        Console.WriteLine("Enter you username: ");
+        inputUsername = Console.ReadLine();
+        Console.WriteLine("Enter your password: ");
+        InputPassword = Console.ReadLine();
+
+      }
+
+      while (!Equals(inputUsername, username) || !Equals(InputPassword, password));
+
+      return true;
+    }
+
+    /// <summary>
+    /// This method has one int parameter.
+    /// It checks if the int is , Console.WriteLine($"{temp} is too cold!");
+    /// between 43 and 78 inclusive, Console.WriteLine($"{temp} is an ok temperature");
+    /// or > 78, Console.WriteLine($"{temp} is too hot!");
+    /// For each temperature range, a different advice is given. 
+    /// </summary>
+    /// <param name="temp"></param>
+    public static void GetTemperatureTernary(int temp)
+    {
+      //throw new NotImplementedException($"GetTemperatureTernary() has not been implemented.");
+      var result2 = temp <= 42 ? $"{temp} is too cold!" :
+        temp >= 43 && temp <= 78 ? $"{temp} is an ok temperature" : $"{temp} is too hot!";
+
+      Console.WriteLine(result2);
+
+    }
+  }//EoP
+}//EoN
---
+++ Coding_Challenges/8_Loops/8_Loops/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8_LoopsChallenge
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      List<int> x = new List<int>();
+      UseFor(x);
+      /* Your code here */
+      List<object> x1 = new List<object>();
+      UseForEach(x1);
+
+      UseWhile(x);
+    }
+
+    /// <summary>
+    /// Return the number of elements in the List<int> that are odd.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public static int UseFor(List<int> x)
+    {
+      // Instantiate the odd variable;
+
+      //throw new NotImplementedException("UseFor() is not implemented yet.");
+      int n = 0;
+      // For each element of y in the list of x, check to see if the remainder of each is an odd integer;
+      foreach (var y in x)
+      {
+        if (y % 2 == 1)
+        {
+          n++;
+        }
+      }
+      return n;
+    }
+
+    /// <summary>
+    /// This method counts the even entries from the provided List<object> 
+    /// and returns the total number found.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public static int UseForEach(List<object> x)
+    {
+      // throw new NotImplementedException("UseForEach() is not implemented yet.");
+      // int evenEntries = 0;
+      // foreach (object entry in x)
+      // {
+      //   if (Convert.ToInt32(entry) % 2 == 0)
+      //   {
+      //     evenEntries++;
+      //   }
+      // }
+      // return evenEntries;
+
+      // Credit goes to austin towler for this block of code
+      // variable counter is set to zero
+      int counter = 0;
+      foreach(object v in x)
+      {
+        if(v is int)
+        {
+          if((int) v % 2 == 0)
+          {
+            counter++;
+          }
+        }
+        if(v is long)
+        {
+          if((long) v % 2 == 0)
+          {
+            counter++;
+          }
+        }
+        if(v is byte)
+        {
+          if((byte) v % 2 == 0)
+          {
+            counter++;
+          }
+        }
+        if(v is uint)
+        {
+          if((uint) v % 2 == 0)
+          {
+            counter++;
+          }
+        }
+        if(v is ulong)
+        {
+          if((ulong) v % 2 == 0)
+          {
+            counter++;
+          }
+        }
+        
+      }
+      return counter;
+
+    }
+
+    /// <summary>
+    /// This method counts the multiples of 4 from the provided List<int>. 
+    /// Exit the loop when the integer 1234 is found.
+    /// Return the total number of multiples of 4.
+    /// </summary>
+    /// <param name="x"></param>
+    public static int UseWhile(List<int> x)
+    {
+      //throw new NotImplementedException("UseFor() is not implemented yet.");
+      int i = 0;
+      int count = 0;
+      bool flag = true;
+      while (flag)
+      {
+        if(x[i] == 1234 || i == x.Count)
+        {
+          break;
+        }
+        if (x[i] % 4 == 0)
+        {
+          count++;
+        }
+        i++;
+      }
+      return count;
+    }
+
+    /// <summary>
+    /// This method will evaluate the Int Array provided and return how many of its 
+    /// values are multiples of 3 and 4.
+    /// </summary>
+    /// <param name="x"></param>
+    /// <returns></returns>
+    public static int UseForThreeFour(int[] x)
+    {
+      counter = 0;
+      index = 0;
+      //throw new NotImplementedException("UseForThreeFour() is not implemented yet.");
+      for(int i = 0; i < x.Count; counter++)
+      {
+        if(x[index] % 3 && x[index] % 4 == 0)
+        {
+          index++;
+        }
+        counter++;
+      }
+      return counter;
+    }
+
+    /// <summary>
+    /// This method takes an array of List<string>'s. 
+    /// It concatenates all the strings, with a space between each, in the Lists and returns the resulting string.
+    /// </summary>
+    /// <param name="stringListArray"></param>
+    /// <returns></returns>
+    public static string LoopdyLoop(List<string>[] stringListArray)
+    {
+      //throw new NotImplementedException("LoopdyLoop() is not implemented yet.");
+      string things ="";
+
+      foreach(var outer in stringListArray)
+      {
+        foreach(var inner in outer)
+        {
+          things += inner + " ";
+        }
+      }
+      return things;
+    }
+  }
+}
