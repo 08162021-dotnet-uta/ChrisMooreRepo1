83c696b80d17b51231fcab86c14ab6ed0845707e
ChristopherMooreRevature <christopher.moore@revature.net>
2021-09-12 18:14:57 -0700

    Edited demo HTML for login page, updating p1

---
+++ projects/project_1/Project1.WebStoreDBContext/Models/ItemizedOrder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Project1.WebStoreDBContext.Models
+{
+    public partial class ItemizedOrder
+    {
+        public Guid ItemizedId { get; set; }
+        public Guid OrderId { get; set; }
+        public int ProductId { get; set; }
+
+        public virtual Order Order { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
---
+++ projects/project_1/Project1.WebStoreDBContext/Models/WebStoreApplicationDBContext.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Project1.WebStoreDBContext.Models
+{
+    public partial class WebStoreApplicationDBContext : DbContext
+    {
+        public WebStoreApplicationDBContext()
+        {
+        }
+
+        public WebStoreApplicationDBContext(DbContextOptions<WebStoreApplicationDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<ItemizedOrder> ItemizedOrders { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<Store> Stores { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=WebStoreApplicationDB;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<ItemizedOrder>(entity =>
+            {
+                entity.HasKey(e => e.ItemizedId)
+                    .HasName("PK__Itemized__AB3A49C52CBDB04E");
+
+                entity.Property(e => e.ItemizedId).HasDefaultValueSql("(newid())");
+
+                entity.HasOne(d => d.Order)
+                    .WithMany(p => p.ItemizedOrders)
+                    .HasForeignKey(d => d.OrderId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__ItemizedO__Order__2F10007B");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.ItemizedOrders)
+                    .HasForeignKey(d => d.ProductId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__ItemizedO__Produ__300424B4");
+            });
+
+            modelBuilder.Entity<Order>(entity =>
+            {
+                entity.Property(e => e.OrderId).HasDefaultValueSql("(newid())");
+
+                entity.Property(e => e.OrderDate).HasColumnType("date");
+
+                entity.HasOne(d => d.Customer)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.CustomerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Orders__Customer__2B3F6F97");
+
+                entity.HasOne(d => d.Store)
+                    .WithMany(p => p.Orders)
+                    .HasForeignKey(d => d.StoreId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Orders__StoreId__2A4B4B5E");
+            });
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.ProductDescription)
+                    .HasMaxLength(100)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ProductName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.ProductPrice).HasColumnType("decimal(19, 2)");
+            });
+
+            modelBuilder.Entity<Store>(entity =>
+            {
+                entity.ToTable("Store");
+
+                entity.Property(e => e.StoreLocation)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StoreName)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
